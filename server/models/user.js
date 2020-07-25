4963f4b20ce64e6faa5d1b9475469ea0ffa1b9fd
iagotd <iagotudela@gmail.com>
2020-07-25 14:36:15 +0200

    New variable, validators and required

--- server/models/user.js
+++ server/models/user.js
@@ -2,8 +2,22 @@ const mongoose = require('mongoose')
 
 const Schema = mongoose.Schema
 const userSchema = new Schema({
-    email: String,
-    password: String
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    userType: {
+        type     : Number,
+        required : true,
+        validate : {
+          validator : Number.isInteger,
+          message   : '{VALUE} is not an integer value'
+        }
+    }
 })
 
 module.exports = mongoose.model('user', userSchema, 'users')
