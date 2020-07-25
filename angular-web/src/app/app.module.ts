2ca6be3649d18d619ffa050c408294c6ff3ed2b3
iagotd <iagotudela@gmail.com>
2020-07-25 14:39:47 +0200

    Added libreries and new components

--- angular-web/src/app/app.module.ts
+++ angular-web/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -8,6 +9,15 @@ import { AboutComponent } from './about/about.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ConfirmComponent } from './confirm/confirm.component';
+import { DataComponent } from './data/data.component';
+import { ControlComponent } from './control/control.component';
+import { AuthService } from './auth.service';
+import { ThingService } from './thing.service';
+import { AuthGuard } from './auth.guard';
+import { DataGuard } from './data.guard';
+import { ControlGuard } from './control.guard';
+import { TokenInterceptorService } from './token-interceptor.service';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
@@ -16,13 +26,23 @@ import { ConfirmComponent } from './confirm/confirm.component';
     AboutComponent,
     LoginComponent,
     RegisterComponent,
-    ConfirmComponent
+    ConfirmComponent,
+    DataComponent,
+    ControlComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [AuthService, ThingService, AuthGuard, DataGuard, ControlGuard,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptorService,
+      multi: true
+    }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
