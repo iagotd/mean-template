9f58a5bfffe0e5c0181d7a77407ad3ac6c1a6cd0
iagotd <iagotudela@gmail.com>
2020-07-25 14:35:45 +0200

    Routing module completed

--- angular-web/src/app/app-routing.module.ts
+++ angular-web/src/app/app-routing.module.ts
@@ -1,11 +1,65 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ConfirmComponent } from './confirm/confirm.component';
+import { DataComponent } from './data/data.component';
+import { ControlComponent } from './control/control.component'
+import { ProfileComponent } from './profile/profile.component'
+import { AuthGuard } from './auth.guard';
+import { DataGuard } from './data.guard';
+import { ControlGuard } from './control.guard';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'data',
+    component: DataComponent,
+    canActivate: [DataGuard]
+  },
+  {
+    path: 'control',
+    component: ControlComponent,
+    canActivate: [ControlGuard]
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: 'confirm',
+    component: ConfirmComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
