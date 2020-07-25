1c0ec1da22ba89c4870c8689bd4ba5e5ddd2636d
iagotd <iagotudela@gmail.com>
2020-07-25 14:40:32 +0200

    Added functions that allow correct visibility

--- angular-web/src/app/app.component.ts
+++ angular-web/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-web';
+  title = 'angular-web'
+
+  constructor(private _authService: AuthService) {}
+
+  logoutUser() {
+    this._authService.logoutUser()
+  }
+
+  isLoggedIn() {
+    return this._authService.userLoggedIn()
+  }
+
+  hasEnoughUserType(requiredUserType) {
+
+    let userTypeNumber = 3;
+    if(!this.isLoggedIn()) return false;
+
+    switch(localStorage.getItem("userType")) {
+      case "user" : {
+        userTypeNumber = 2
+        break
+      }
+      case "premium" : {
+        userTypeNumber = 1
+        break
+      }
+      case "admin" : {
+        userTypeNumber = 0
+        break
+      }
+      default: userTypeNumber = 3
+    }
+    if(userTypeNumber > requiredUserType) {
+      return false
+    } else {
+      return true
+    }
+  }
+
 }
