f9c53a46d0a461e9d5fa27e7368cc3b769132acc
iagotd <iagotudela@gmail.com>
2020-07-27 15:10:44 +0200

    Confirm component finished

--- angular-web/src/app/confirm/confirm.component.ts
+++ angular-web/src/app/confirm/confirm.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-confirm',
@@ -7,9 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConfirmComponent implements OnInit {
 
-  constructor() { }
+  confirmText = "An unexpected error has occurred.";
+  confirmData = {email: '', token: ''};
+  constructor(private _route: ActivatedRoute,
+              private _auth: AuthService,
+              private _router: Router) { }
 
   ngOnInit(): void {
-  }
 
+    this.confirmData.email = this._route.snapshot.paramMap.get('email');
+    this.confirmData.token = this._route.snapshot.paramMap.get('token');
+
+    if (this.confirmData.email !== null && this.confirmData.token !== null) {
+      this._auth.confirmUser(this.confirmData)
+      .subscribe(
+        res => { 
+          localStorage.setItem('justConfirmed', "true")
+          this._router.navigate(['/confirm'])
+        },
+        err => console.log(err)
+      );
+    }
+
+    let justReg = localStorage.getItem("justRegistered");
+    let justConf = localStorage.getItem("justConfirmed");
+
+    if(justConf === "true") {
+      this.confirmText = "Account activated. Please, login to use our services.";
+    } else if (justReg === "true") {
+      this.confirmText = "Email sent. Please, activate your account to use our services";
+    } else {
+      this._router.navigate(['/home'])
+    }
+  }
 }
