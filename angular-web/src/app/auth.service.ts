bf68558222ea1b5ed99a2d358e25a2a1757d5e31
iagotd <iagotudela@gmail.com>
2020-07-28 23:24:39 +0200

    Added verifyToken function to be used by guards

--- angular-web/src/app/auth.service.ts
+++ angular-web/src/app/auth.service.ts
@@ -1,18 +1,37 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router} from '@angular/router'
+import { of, Observable } from "rxjs";
+import { tap, map } from "rxjs/operators";
+
 @Injectable()
 export class AuthService {
 
   private _registerUrl = "https://localhost:8443/api/register";
   private _loginUrl = "https://localhost:8443/api/login";
   private _confirmUrl = "https://localhost:8443/api/confirmation";
+  private _verifyTokenUrl = "https://localhost:8443/api/verify-token";
 
   constructor(private http: HttpClient,
               private _router: Router) { }
 
   registerUser(user) {
     return this.http.post<any>(this._registerUrl, user)
+  }
+
+  verifyToken(): Observable<boolean> {
+    const token = this.getToken();
+    return token ? this.http
+      .post(this._verifyTokenUrl, {
+        'token': localStorage.getItem('token'),
+        'userType': localStorage.getItem('userType')
+      })
+      .pipe(
+        map(
+          res => true,
+          error => false
+        )
+      ) : of(false)
   }
 
   confirmUser(confirmData) {
@@ -35,6 +54,7 @@ export class AuthService {
   premiumLoggedIn() {
     let userType = localStorage.getItem('userType')
     if (userType === "premium" || userType === "admin") {
+      //ask server if it is OK
       return true
     } else {
       return false
